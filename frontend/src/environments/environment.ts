6ec9efc1aa083b6db53f9c5c14c029370a916c53
RayanAlkhelaiwi <r.n.m.k@live.com>
2020-09-26 11:06:23 +0300

    Fix domain prefix
    For auth0

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'https://rayan-dev.us.auth0.com/', // the auth0 domain prefix
+    url: 'rayan-dev.us', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'RYGyvUmOSZXpx58l5xul3eTvUixMi3Xl', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
